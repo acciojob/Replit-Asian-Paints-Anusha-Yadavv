fd4fde3edbecc748450a50c59607779f2c900c75
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-27 21:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+ function changeColor() {
+        const blockId = document.getElementById("block_id").value;
+        const color = document.getElementById("colour_id").value;
+
+        const blockElement = document.getElementById(blockId);
+        if (blockElement) {
+            document.querySelectorAll('.grid-item').forEach(item => {
+                item.style.backgroundColor = 'transparent';
+            });
+            blockElement.style.backgroundColor = color;
+        } else {
+            alert("Invalid Block ID");
+        }
+    }
+
+    function resetColors() {
+        document.querySelectorAll('.grid-item').forEach(item => {
+            item.style.backgroundColor = 'transparent';
+        });
+    }
